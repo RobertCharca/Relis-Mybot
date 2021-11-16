871468f2f15c44e8bcb200dc085bb4df021f4272
Robert Charca Cueva <trevoraobac@gmail.com>
2021-11-15 23:49:41 -0500

    _pycache_part 2

--- RelisMybot/settings.py
+++ RelisMybot/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'RelisMybot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bd de psicología',
+        'USER': 'eduardo',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
