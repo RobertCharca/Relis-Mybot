831271bf09d15e6c2debd1b8090162360e77e6fa
Robert Charca Cueva <trevoraobac@gmail.com>
2021-11-16 15:45:19 -0500

    Connection to 'Alumnos' database

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
+
+def login(request):
+    login = AuthenticationForm()
+    return render(request, 'login.html', {'login':login})
+
+def register(request):
+    form = UserCreationForm()
+    return render(request, 'register.html', {'form':form})
