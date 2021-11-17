92ea7adb3653d299a0aeb4b2aab5d4d18dfb68e5
Robert Charca Cueva <trevoraobac@gmail.com>
2021-11-16 23:08:22 -0500

    'register' function added. Registration form works fine :)

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
+from django.contrib import messages
+from .forms import CreateUserForm
 # Create your views here.
 
 def login(request):
@@ -9,5 +10,10 @@ def login(request):
     return render(request, 'login.html', {'login':login})
 
 def register(request):
-    form = UserCreationForm()
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Se registró correctamente el alumno')
     return render(request, 'register.html', {'form':form})
